6a11ac6bab53b7d04a4199b0958241b5be7ebc31
Harshit <harshitchugh001@gmail.com>
2024-03-21 16:13:18 +0530

    script added

--- tests/course.sh
+++ tests/course.sh
@@ -26,38 +26,51 @@ IDENTITIES=("principal2" "principal3" "principal4" "principal5")
 
 declare -A course_data
 
-course_data[principal2]='record {
+
+
+dfx canister call  backend_content_canister addCourse '(record {
     courseTitle = "ICP Developer Course: Mastering Smart Contract Deployment";
     courseImg= "img";
     shortdescription="This intensive, self-paced course is tailored for individuals eager to dive deep into smart contract development on the Internet Computer (ICP). Over two weeks, participants will learn the essentials of setting up their development environment, understand the basics of ICPs Azle, and gain hands-on experience by deploying their first canister smart contract.";
     longdescription="This intensive, self-paced course is tailored for individuals eager to dive deep into smart contract development on the Internet Computer (ICP). Over two weeks, participants will learn the essentials of setting up their development environment, understand the basics of ICPs Azle, and gain hands-on experience by deploying their first canister smart contract.";
     videocount= 0;
-    videoidlist=[""];
-    certificateimg=Text;
+    videoidlist= opt record  { "hhh"};
+    certificateimg="hhh";
     duration= 0;
     level="Beginner";
     viewcount=0;
-    viewlist=[""];
+    viewlist= opt record { "hhh" };
     enrollmentcount=0;
-    enrollmentuserId=[""];
+    enrollmentuserId= null;
     rating=5;
-    learningpoints=[""];
-    questions=[""];
-    coursetype=Self-Paced with Practical Exercises;
+    learningpoints= opt record  {"hhh"};
+    questions= opt record {"h" };
+    coursetype="Self-Paced with Practical Exercises";
     professorName ="Harshit";
-    professorId = "123445";
-}'
+    professorId = "123445";})'
 
-for IDENTITY in "${!course_data[@]}"
-do
-  dfx identity use "$IDENTITY"
-  CURRENT_PRINCIPAL=$(dfx identity get-principal)
-  echo "Using identity $IDENTITY with principal $CURRENT_PRINCIPAL"
 
-  # Extract and use the user data for the current identity
-  COURSE_DATA=${course_data[$IDENTITY]}
-  echo "Registering with data: $COURSE_DATA"
 
-  # Call the register_user function with the current identity and its unique data
-  dfx canister call $CANISTER addCourse "($USER_DATA)"
-done
+
+
+
+
+# for IDENTITY in "${!course_data[@]}"
+# do
+#   dfx identity use "$IDENTITY"
+#   CURRENT_PRINCIPAL=$(dfx identity get-principal)
+#   echo "Using identity $IDENTITY with principal $CURRENT_PRINCIPAL"
+
+#   # Extract and use the user data for the current identity
+#   COURSE_DATA=${course_data[$IDENTITY]}
+#   echo "Registering with data: $COURSE_DATA"
+
+#   # Call the register_user function with the current identity and its unique data
+#   dfx canister call $CANISTER addCourse "($USER_DATA)"
+# done
+
+
+dfx canister call $CANISTER getallCourse
+
+dfx canister call $CANISTER getfullCourse '("1711014546691834685")'
+
