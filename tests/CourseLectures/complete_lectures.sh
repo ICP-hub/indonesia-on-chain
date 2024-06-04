1d3f82ec30be84996c46db30cf39bbdfb6150c7a
codesclown <chandan.vishwakarma@quadbtech.com>
2024-06-04 11:10:28 +0530

    change in code

--- tests/CourseLectures/complete_lectures.sh
+++ tests/CourseLectures/complete_lectures.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Define a variable to hold the data
-CourseID="1717397628339224269"
+CourseID="1717406180038154874"
 
 # Execute each script with the CourseID variable as an argument
 ./lectures/lecture1.sh $CourseID &&
