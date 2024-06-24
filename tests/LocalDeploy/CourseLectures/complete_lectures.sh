1c6667c17d1138238d0b0d4db02f783e8fb43bd5
codesclown <chandan.vishwakarma@quadbtech.com>
2024-06-24 17:28:35 +0530

    change in code

--- tests/LocalDeploy/CourseLectures/complete_lectures.sh
+++ tests/LocalDeploy/CourseLectures/complete_lectures.sh
@@ -4,18 +4,18 @@
 set -e
 
 # Define a variable to hold the data
-CourseID="1719069120836264681"
+CourseID="1719209879052740050"
 
 # Execute each script with the CourseID variable as an argument
 ./lectures/lecture1.sh $CourseID &&
-./lectures/lecture2.sh $CourseID &&
-./lectures/lecture3.sh $CourseID &&
-./lectures/lecture4.sh $CourseID &&
-./lectures/lecture5.sh $CourseID &&
-./lectures/lecture6.sh $CourseID &&
-./lectures/lecture7.sh $CourseID &&
-./lectures/lecture8.sh $CourseID &&
-./lectures/lecture9.sh $CourseID &&
-./lectures/lecture10.sh $CourseID &&
-./lectures/lecture11.sh $CourseID &&
-./lectures/lecture12.sh $CourseID
+./lectures/lecture2.sh $CourseID
+# ./lectures/lecture3.sh $CourseID &&
+# ./lectures/lecture4.sh $CourseID &&
+# ./lectures/lecture5.sh $CourseID &&
+# ./lectures/lecture6.sh $CourseID &&
+# ./lectures/lecture7.sh $CourseID &&
+# ./lectures/lecture8.sh $CourseID &&
+# ./lectures/lecture9.sh $CourseID &&
+# ./lectures/lecture10.sh $CourseID &&
+# ./lectures/lecture11.sh $CourseID &&
+# ./lectures/lecture12.sh $CourseID
