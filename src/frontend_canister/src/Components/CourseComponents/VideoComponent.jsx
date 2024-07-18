9b946f5745c347a7af024c75b0e24803b8a6044a
codesclown <chandan.vishwakarma@quadbtech.com>
2024-07-18 17:15:58 +0530

    change in code

--- src/frontend_canister/src/Components/CourseComponents/VideoComponent.jsx
+++ src/frontend_canister/src/Components/CourseComponents/VideoComponent.jsx
@@ -5,7 +5,7 @@ export default function VideoComponent({ videoUrl }) {
   return (
     <div className="video-wrapper rounded-lg overflow-hidden">
       <ReactPlayer
-        url="https://storage.googleapis.com/ioc-data/Kenny%20-%20NFID.mp4"
+        url={videoUrl}
         controls={true}
         width="100%"
         height="550px"
