f68f24344a32f16d05fc64981cb6483bae57b306
codesclown <chandan.vishwakarma@quadbtech.com>
2024-10-15 02:24:30 +0530

    change  in code

--- src/frontend_canister/src/Components/DashBoardComponents/components/DashboardMobileTabPanel.jsx
+++ src/frontend_canister/src/Components/DashBoardComponents/components/DashboardMobileTabPanel.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import DashboardOngoingCourseComponent from "./DashboardOngoingCourseComponent";
 import DashboardRecommededCourse from "./DashboardRecommededCourse";
 import { useTranslation } from "react-i18next";
+import { useAuth } from "../../utils/useAuthClient";
 const DashboardMobileTabPanel = () => {
   const { t } = useTranslation();
+  const { contentActor } = useAuth();
+  const [Loading, setLoading] = useState(true);
+  const [recommendedCourses, setRecommendedCourses] = useState([]);
+
+  useEffect(() => {
+    // dispatch({type:'CHECK_USER_PRESENT'});
+    const fetchData = async () => {
+      try {
+        const user = await contentActor.getallCourse();
+        // console.log("courses recived as from backend", user);
+        const courses = user.leaf.keyvals[0][0].slice(1);
+        let number = parseInt(user.leaf.size);
+        // console.log(number);
+
+        const newData = [];
+        for (let i = 0; i < number; i++) {
+
+          let time = 0;
+          let newCourse = user.leaf.keyvals;
+          while (time < i) {
+            newCourse = newCourse[0][1];
+            time++;
+          }
+          newCourse = newCourse[0][0][1];
+          newData.push(newCourse);
+        }
+        setRecommendedCourses(newData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, []);
   return (
     <Tabs className="pb-8">
       <TabList className="flex gap-2 p-1 m-1 space-x-1 rounded-xl">
@@ -24,7 +64,7 @@ const DashboardMobileTabPanel = () => {
       </TabPanel>
       <TabPanel>
         <div className="flex items-center justify-center w-full my-8">
-          <DashboardRecommededCourse />
+          <DashboardRecommededCourse  recommendedCourses={recommendedCourses} />
         </div>
       </TabPanel>
     </Tabs>
